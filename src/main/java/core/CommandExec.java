9333d4b54232c609e006888dd1e7ae204359028b
vcbumg2 <cody@uky.edu>
2015-11-03 13:08:19 -0500

    Woot

--- src/main/java/core/CommandExec.java
+++ src/main/java/core/CommandExec.java
@@ -72,6 +72,25 @@ public class CommandExec {
 							String[] cmd = {"sh", "(sleep 10; /usr/sbin/service/service /etc/init.d/cresco-controller upgrade) &"};
 							Process p=Runtime.getRuntime().exec(cmd);
 							
+							BufferedReader stdInput = new BufferedReader(new 
+								     InputStreamReader(p.getInputStream()));
+
+								BufferedReader stdError = new BufferedReader(new 
+								     InputStreamReader(p.getErrorStream()));
+
+								// read the output from the command
+								System.out.println("Here is the standard output of the command:\n");
+								String s = null;
+								while ((s = stdInput.readLine()) != null) {
+								    System.out.println(s);
+								}
+
+								// read any errors from the attempted command
+								System.out.println("Here is the standard error of the command (if any):\n");
+								while ((s = stdError.readLine()) != null) {
+								    System.out.println(s);
+								}
+							
 							//Process p=Runtime.getRuntime().exec("(sleep 10; service cresco-controller upgrade) &");
 							ce.setMsgBody("updating global controller");
 							
